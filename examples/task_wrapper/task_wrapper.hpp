2fd7742b6b446c4f06bfb905543ec2c79904281b
Konstantin Morozov <morozov-kst@yandex.ru>
2023-01-13 09:25:46 +0400

    inti example

--- examples/task_wrapper/task_wrapper.hpp
+++ examples/task_wrapper/task_wrapper.hpp
@@ -7,33 +7,40 @@
 
 #include "vtable_for.h"
 
+template <typename Signature>
+class TaskWrapper;
+
 template <typename R, typename... Args>
-class TaskWrapper final {
+class TaskWrapper<R(Args...)> final {
   public:
     TaskWrapper() = default;
 
     template <class Callable>
     explicit TaskWrapper(Callable c) : vtable_{&vtable_for<Callable, R, Args...>} {
         static_assert(sizeof(Callable) <= sizeof(buf_), "small buffer for callable object");
-        new (&buf_) Callable{std::move(c)};
+        new (access()) Callable{std::move(c)};
     }
 
     R operator()(Args... args) {
         if (vtable_) {
-            return vtable_->run(&buf_, args...);
+            return vtable_->run(access(), args...);
         }
         throw std::invalid_argument("broken vtable");
     }
 
     ~TaskWrapper() {
         if (vtable_) {
-            vtable_->destroy(&buf_);
+            vtable_->destroy(access());
         }
     }
 
   private:
     std::aligned_storage_t<64> buf_{};
     const detail::vtable<R, Args...>* vtable_{};
+
+    auto* access() {
+        return &buf_;
+    }
 };
 
 #endif // ARCANUM_TASK_WRAPPER_HPP
