fd6b9d31d91b5ff104077c493e450240cfe171c0
Konstantin Morozov <morozov-kst@yandex.ru>
2023-01-13 09:50:05 +0400

    add store

--- examples/task_wrapper/task_wrapper.hpp
+++ examples/task_wrapper/task_wrapper.hpp
@@ -12,35 +12,41 @@ class TaskWrapper;
 
 template <typename R, typename... Args>
 class TaskWrapper<R(Args...)> final {
+    struct StoreManager {
+        std::aligned_storage_t<64> buf_{};
+        auto* access() {
+            return &buf_;
+        }
+        auto& buf() {
+            return buf_;
+        }
+    };
+
   public:
     TaskWrapper() = default;
 
     template <class Callable>
     explicit TaskWrapper(Callable c) : vtable_{&vtable_for<Callable, R, Args...>} {
-        static_assert(sizeof(Callable) <= sizeof(buf_), "small buffer for callable object");
-        new (access()) Callable{std::move(c)};
+        static_assert(sizeof(Callable) <= sizeof(store_.buf()), "small buffer for callable object");
+        new (store_.access()) Callable{std::move(c)};
     }
 
     R operator()(Args... args) {
         if (vtable_) {
-            return vtable_->run(access(), args...);
+            return vtable_->run(store_.access(), args...);
         }
         throw std::invalid_argument("broken vtable");
     }
 
     ~TaskWrapper() {
         if (vtable_) {
-            vtable_->destroy(access());
+            vtable_->destroy(store_.access());
         }
     }
 
   private:
-    std::aligned_storage_t<64> buf_{};
+    StoreManager store_{};
     const detail::vtable<R, Args...>* vtable_{};
-
-    auto* access() {
-        return &buf_;
-    }
 };
 
 #endif // ARCANUM_TASK_WRAPPER_HPP
